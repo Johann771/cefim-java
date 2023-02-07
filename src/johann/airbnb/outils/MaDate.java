9da1cb554a17439e374c8f86613342fdd91e4b0f
johannweytens <weytensjohann771@gmail.com>
2023-02-07 15:22:36 +0100

    Polymorphisme

--- src/johann/airbnb/outils/MaDate.java
+++ src/johann/airbnb/outils/MaDate.java
@@ -7,9 +7,12 @@ public class MaDate extends java.util.Date {
 
     private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy");
 
-    public MaDate(long date) {
-        super(date);
-    }
+    /**
+     * @author Johann Weytens
+     * @param jour jour entre 1 et 31
+     * @param mois mois entre 1 et 12
+     * @param annee annee du style 2023
+     */
     public MaDate(int jour, int mois, int annee){
         super(annee-1900,mois-1,jour);
     }
